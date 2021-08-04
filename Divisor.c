16bbe5a9fd8be93ccc71d0330f53c6c45775de08
saehyen <rlatpgus4564@naver.com>
2021-08-04 13:02:31 +0900

    프로그래머스_약수의합

---
+++ Divisor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i < n + 1; i++) {
+        if ((n % i) == 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
